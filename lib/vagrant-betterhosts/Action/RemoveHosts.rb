4c240f46883d516fdcd631cec1d69961e7bcaf07
Adam Butler <Adam.J.Butler@protonmail.com>
2021-06-06 00:26:45 +0100

    Ensure hosts are removed on destroy, halt, and suspend

--- lib/vagrant-betterhosts/Action/RemoveHosts.rb
+++ lib/vagrant-betterhosts/Action/RemoveHosts.rb
@@ -5,15 +5,15 @@ module VagrantPlugins
 
         def run(env)
           machine_action = env[:machine_action]
-          if machine_action != :destroy || !@machine.id
-            if machine_action != :suspend || false != @machine.config.betterhosts.remove_on_suspend
-              if machine_action != :halt || false != @machine.config.betterhosts.remove_on_suspend
-                @ui.info "[vagrant-betterhosts] Removing hosts"
-                removeHostEntries
-              else
-                @ui.info "[vagrant-betterhosts] Removing hosts on suspend disabled"
-              end
-            end
+
+          return unless @machine.id
+          return unless %i[destroy halt suspend].include? machine_action
+
+          if (%i[halt suspend].include? machine_action) && (false == @machine.config.goodhosts.remove_on_suspend)
+            @ui.info '[vagrant-betterhosts] Removing hosts on suspend disabled'
+          else
+            @ui.info '[vagrant-betterhosts] Removing hosts'
+            removeHostEntries
           end
         end
 
--- lib/vagrant-betterhosts/config.rb
+++ lib/vagrant-betterhosts/config.rb
@@ -3,14 +3,15 @@ require "vagrant"
 module VagrantPlugins
   module BetterHosts
     class Config < Vagrant.plugin("2", :config)
-        attr_accessor :aliases
-        attr_accessor :id
-        attr_accessor :remove_on_suspend
-        attr_accessor :disable_clean
-        
-        def initialize
-            @disable_clean = true
-        end
+      attr_accessor :aliases
+      attr_accessor :id
+      attr_accessor :remove_on_suspend
+      attr_accessor :disable_clean
+
+      def initialize
+        @remove_on_suspend = true
+        @disable_clean = true
+      end
     end
   end
 end
